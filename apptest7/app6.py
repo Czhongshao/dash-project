6d7be16800f8492151ddfa199dc6f22a06d5c54a
zhongshao <145018310+Czhongshao@users.noreply.github.com>
2025-01-01 14:36:52 +0800

    删除多余文件

--- apptest7/app6.py
+++ apptest7/app6.py
@@ -3,12 +3,11 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 from sqlalchemy import create_engine
 
-# 确保数据库URL、用户名和密码是正确的
-postgres_url = 'mysql+pymysql://@localhost:3306/onlinetradingsystem'
-engine = create_engine(postgres_url)
+sql_name = "onlinetradingsystem"
+mysql_engine = create_engine(f'mysql+pymysql://root:666666@localhost:3306/{sql_name}')
 
 app = dash.Dash(__name__)
 
@@ -17,16 +16,17 @@ app.layout = html.Div(
         [
             dbc.Row(
                 [
-                    dbc.Col(dbc.Button('更新数据库信息', id='refresh-db', style={'width': '100%'}), width=2),
-                    dbc.Col(dcc.Dropdown(id='db-table-names', placeholder='选择库中数据表', style={'width': '100%'}), width=4),
-                    dbc.Col(dbc.Button('查询', id='query', style={'width': '100%'}), width=1)
+                    dbc.Col(dbc.Button(f"更新当前数据库{sql_name}", id='refresh-db', style={'width': '100%'}), width=2),
+                    dbc.Col (dcc.Dropdown (id='db-table-names', placeholder='选择库中数据表', style={'width': '100%'}),
+                             width=4),
+                    dbc.Col (dbc.Button ('查询', id='query', style={'width': '100%'}), width=1)
                 ]
             ),
             html.Hr(),
             dbc.Row(
                 [
                     dbc.Col(
-                        id='query-result'
+                        id="query-result"
                     )
                 ]
             )
@@ -38,31 +38,16 @@ app.layout = html.Div(
 )
 
 @app.callback(
-    Output('db-table-names', 'options'),
-    Input('refresh-db', 'n_clicks'),
+    Output("db-table-names", "options"),
+    Input("refresh-db", "n_clicks"),
     prevent_initial_call=True
 )
-def refresh_table_names(n_clicks):
-    try:
-        table_names_query = "SELECT  FROM onlinetradingsystem.users WHERE table_schema = 'public'"
-        table_names = pd.read_sql_query(table_names_query, con=engine)
-        return [{'label': name, 'value': name} for name in table_names['table_name']]
-    except Exception as e:
-        print(e)
-        return [{'label': '无法连接到数据库', 'value': ''}]
 
-@app.callback(
-    Output('query-result', 'children'),
-    Input('query', 'n_clicks'),
-    State('db-table-names', 'value'),
-    prevent_initial_call=True
-)
-def query_data_records(n_clicks, value):
-    if value:
-        query_result = pd.read_sql_query(f'SELECT * FROM `{value}` LIMIT 500', con=engine)
-        return html.Div(dbc.Table.from_dataframe(query_result, striped=True), style={'height': '600px', 'overflow': 'auto'})
-    else:
-        return dash.no_update
+def refresh_db(n_clicks):
+    table_names = pd.read_sql_query(f"select table_name from onlinetradingsystem.users where  table_schema='public'", con=mysql_engine)
+    return [{'label': name, 'value': name} for name in table_names['tablename']]
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
